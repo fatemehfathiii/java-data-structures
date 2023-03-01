baa28ddda75715c7d392c1647cf2021d23e40fbb
fatemeh.fathi <fatemehfathi409@gmail.com>
2023-03-01 15:05:42 +0330

    add doubly and circular linked list

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
-import containers.List;
-import containers.arrayList.MyArrayList;
-import containers.linkedList.MySinglyLinkedList;
+import containers.linkedList.MyDoublyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
-        MySinglyLinkedList<String> list = new MySinglyLinkedList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
-        list.add("e");
-        list.addLast("f");
-        list.addLast("g");
-        list.print();
-        list.deleteFirst();
-        list.print();
-        list.deleteLast();
-        list.print();
-
-
 
     }
 }
